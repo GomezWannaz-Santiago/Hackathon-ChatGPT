9cc398ec61c4a8557d256f547bc32b8c2c8f30a3
GomezWannaz-Santiago <santiagogomezwannaz@gmail.com>
2023-05-20 13:37:09 -0300

    Models de email

--- FrontHackathon/src/app/models/email-validate.ts
+++ FrontHackathon/src/app/models/email-validate.ts
@@ -1,7 +1,12 @@
 export interface ValidateEmailRequest{
+    text : string;
+    desconocido : boolean;
+    tieneMuchosLinks : boolean;
 
 }
 
 export interface ValidateEmailResponse{
-    
+    text : string;
+    isSpam : boolean;
+
 }
